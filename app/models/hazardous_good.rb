cecbe16f8be56a7ba7d032c1cef9aef27850276c
Steven Diviney <sdiviney@zendesk.com>
2020-12-29 14:36:31 -0800

    Hazardous to_s

--- app/models/hazardous_good.rb
+++ app/models/hazardous_good.rb
@@ -1,3 +1,7 @@
 class HazardousGood < ApplicationRecord
   has_many :docket
+
+  def to_s
+    "#{un_number} / #{name} / #{primary_class} / #{secondary_class} / #{packing_group} / #{tunnel_code}"
+  end
 end
