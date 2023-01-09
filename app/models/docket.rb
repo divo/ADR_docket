1026ae918abf91ea748380fc234573932b9a4773
divo <divoapple@gmail.com>
2023-01-09 13:02:26 +0000

    Remove account from docket for now

--- app/models/docket.rb
+++ app/models/docket.rb
@@ -2,5 +2,4 @@ class Docket < ApplicationRecord
   belongs_to :deliver_to, class_name: 'Address', optional: true
   belongs_to :collect_from, class_name: 'Address', optional: true
   belongs_to :hazardous_good, optional: true
-  belongs_to :account
 end
