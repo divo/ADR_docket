77147e4588fcac5d357ec2150d2d7a3dc37d8255
divo <divoapple@gmail.com>
2023-01-14 15:39:43 +0000

    Execute rails-playbook on host

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -15,6 +15,7 @@ import pulumi_aws as aws
 import pulumi_command as command
 import base64
 from pathlib import Path
+import checksumdir
 
 config = pulumi.Config()
 
@@ -42,6 +43,10 @@ secgrp = aws.ec2.SecurityGroup('secgrp',
         ingress=[
             aws.ec2.SecurityGroupIngressArgs(protocol='tcp', from_port=22, to_port=22, cidr_blocks=['0.0.0.0/0']),
             aws.ec2.SecurityGroupIngressArgs(protocol='tcp', from_port=80, to_port=80, cidr_blocks=['0.0.0.0/0']),
+            aws.ec2.SecurityGroupIngressArgs(protocol='tcp', from_port=443, to_port=443, cidr_blocks=['0.0.0.0/0']),
+            ],
+        egress=[
+            aws.ec2.SecurityGroupIngressArgs(protocol='-1', from_port=0, to_port=0, cidr_blocks=['0.0.0.0/0']),
             ],
         )
 
@@ -55,6 +60,21 @@ connection = command.remote.ConnectionArgs(
     host=server.public_ip,
     user='ec2-user',
     private_key=privateKey,
+)
+
+# Hash the dirs contents to determine if an update is needed.
+# TODO: This just triggers everytime. write it out to a file and compare or something
+checksum = checksumdir.dirhash('../app', 'md5')
+
+# Play the Ansible playbook
+play_ansible_playbook_cmd = command.local.Command("playAnsiblePlaybookCmd",
+    create=server.public_ip.apply(lambda public_ip: f"""\
+            ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_PYTHON_INTERPRETER=auto_silent ansible-playbook \
+            -u ec2-user \
+            -i '{public_ip},' \
+            --private-key {privateKeyPath} \
+            rails-playbook.yml"""),
+    triggers=[checksum],
 )
 
 pulumi.export('publicIp', server.public_ip)
