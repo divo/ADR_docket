7473e387f199a2612702d26e10d8e7f689f6837a
divo <divoapple@gmail.com>
2023-01-17 13:38:48 +0000

    Basic login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to login_url, alert: "Invalid email or password"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out"
   end
 end
