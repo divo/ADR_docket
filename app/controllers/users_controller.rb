ab9caba78182db23749d14c39b659c75fd9162ba
divo <divoapple@gmail.com>
2023-01-23 09:41:58 +0000

    Display newly created account details in toast

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-        format.html { redirect_to dockets_url, notice: "User was successfully created." }
+        format.html { redirect_to dockets_url, notice: "#{@user.email} was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
