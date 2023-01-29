f054131998a758e40fe788fca319810e406205aa
divo <divoapple@gmail.com>
2023-01-29 19:22:06 +0000

    Analytics client, wrapped in a job.
    Not using the frontend to send analytics for now as I have no need

---
+++ app/jobs/analytics_job.rb
@@ -0,0 +1,13 @@
+class AnalyticsJob < ApplicationJob
+  queue_as :default
+
+  def perform(created_at, session_id, user_id, event)
+    AnalyticsClient.new().send_analytics(
+      url: Rails.application.credentials[:analytics_url],
+      created_at: created_at,
+      session_id: session_id,
+      user_id: user_id,
+      events: [event]
+    )
+  end
+end
