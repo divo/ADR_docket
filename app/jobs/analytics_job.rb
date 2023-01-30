4bc026d5def3804729ebf842ea79c8b024041471
divo <divoapple@gmail.com>
2023-01-30 11:42:05 +0000

    Fix namespace error

--- app/jobs/analytics_job.rb
+++ app/jobs/analytics_job.rb
@@ -2,7 +2,7 @@ class AnalyticsJob < ApplicationJob
   queue_as :default
 
   def perform(created_at, session_id, user_id, event)
-    AnalyticsClient.new().send_analytics(
+    ::AnalyticsClient.new().send_analytics(
       url: Rails.application.credentials[:analytics_url],
       created_at: created_at,
       session_id: session_id,
